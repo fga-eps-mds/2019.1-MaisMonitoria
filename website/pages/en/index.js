e1a15c3ae95280dccbf72f9f33a918003ad2b6ee
matheus-rn <matheus.1nascimento58@gmail.com>
2019-04-12 11:30:29 -0300

    Ajustada base git-pages

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -12,7 +12,7 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
-
+	
 class HomeSplash extends React.Component {
   render() {
     const {siteConfig, language = ''} = this.props;
@@ -74,134 +74,98 @@ class HomeSplash extends React.Component {
   }
 }
 
-class Index extends React.Component {
-  render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
 
-    const Block = props => (
-      <Container
-        padding={['bottom', 'top']}
-        id={props.id}
-        background={props.background}>
-        <GridBlock
-          align="center"
-          contents={props.children}
-          layout={props.layout}
-        />
-      </Container>
-    );
-
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
-    );
-
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content: 'Talk about trying this out',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'left',
-            title: 'Try it Out',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content: 'Talk about learning how to use this',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Learn How',
-          },
-        ]}
-      </Block>
-    );
-
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
-          },
-        ]}
-      </Block>
-    );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <div>
-            <a href={user.infoLink} key={user.infoLink}>
-              <img src={user.image} alt={user.caption} title={user.caption}/>
-            </a>
-						<h4>{user.caption}</h4>
-						<p>{user.role}</p>
-          </div>
-          ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Este projeto foi idealizado e está sendo desenvolvido por:</h2>
-          <div className="logos">{showcase}</div>
-        </div>
-      );
-    };
-
+class Meio extends React.Component {
+  render(){
     return (
       <div>
-        {/* <HomeSplash siteConfig={siteConfig} language={language} /> */}
-        <h1 align='center'>Em construção...</h1>
-        <div className="mainContainer">
-          {/* <Features /> */}
-          {/* <FeatureCallout /> */}
-          {/* <LearnHow /> */}
-          {/* <TryOut /> */}
-          {/* <Description /> */}
-          <Showcase />
+      <section className="about-area section-gap">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-lg-5 col-md-6 about-left">
+              <img className="img-fluid" src="img/about.jpg" alt=""/>
+            </div>
+            <div className="offset-lg-1 col-lg-6 offset-md-0 col-md-12 about-right">
+              <h1>
+                Over 2500 Courses <br/> from 5 Platform
+              </h1>
+              <div className="wow fadeIn" data-wow-duration="1s">
+                <p>
+                  There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think
+                  about setting up your own viewing station. In the life of any aspiring astronomer that it is time to buy that first
+                  telescope. It’s exciting to think about setting up your own viewing station.
+                </p>
+              </div>
+              <a href="courses.html" className="primary-btn">Explore Courses</a>
+            </div>
+          </div>
         </div>
+      </section>
       </div>
     );
   }
 }
+
+class Membros extends React.Component {
+  render(){
+    return(
+        <section className="faculty-area section-gap">
+		      <div className="container">
+			      <div className="row justify-content-center">
+				      <div className="col-lg-8">
+					      <div className="section-title text-center">
+						      <h1>Membros</h1>
+					      </div>
+				      </div>
+			      </div>
+            <div className="row justify-content-center d-flex align-items-center">
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Ethel Davis</h4>
+                  <p className="designation">Sr. Faculty Data Science</p>
+                </div>
+              </div>
+            </div>
+          </div>
+	      </section>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    return (
+        <div>
+          <div>
+          <section className="home-banner-area relative" style={{height: "37.2em"}}>
+            <div className="container">
+              <div className="row fullscreen d-flex align-items-center justify-content-center">
+                <div className="banner-content col-lg-8 col-md-12">
+                  <h1 className="wow fadeIn" data-wow-duration="4s">We Rank the Best Courses <br/> on the Web</h1>
+                  <p className="text-white">
+                    In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space
+                    telescope.
+                  </p>
+                </div>
+              </div>
+            </div>
+              <div className="rocket-img">
+                <img src="img/rocket.png" alt=""/>
+              </div>
+          </section>
+          </div>
+          <Meio />
+          <Membros />
+          <div id="back-top">
+		        <a title="Go to Top" href="#"></a>
+	        </div>
+        </div>
+    );
+  }
+}
+
 
 module.exports = Index;
