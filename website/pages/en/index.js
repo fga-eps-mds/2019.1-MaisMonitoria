0d562449372ef0207e1309be6a37f8d80f48a901
lukassxp <lpalucas.10@gmail.com>
2019-04-12 17:03:53 -0300

    [#23] Ajustados alguns pontos do layout.

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -12,68 +12,6 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
-	
-class HomeSplash extends React.Component {
-  render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
-    );
-
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
-
-    return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/docusaurus.svg`} />
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection>
-        </div>
-      </SplashContainer>
-    );
-  }
-}
-
 
 class Meio extends React.Component {
   render(){
@@ -87,7 +25,7 @@ class Meio extends React.Component {
             </div>
             <div className="offset-lg-1 col-lg-6 offset-md-0 col-md-12 about-right">
               <h1>
-                Over 2500 Courses <br/> from 5 Platform
+                The standard <br/> Lorem Ipsum
               </h1>
               <div className="wow fadeIn" data-wow-duration="1s">
                 <p>
@@ -96,7 +34,7 @@ class Meio extends React.Component {
                   telescope. It’s exciting to think about setting up your own viewing station.
                 </p>
               </div>
-              <a href="courses.html" className="primary-btn">Explore Courses</a>
+              <a href="#" className="primary-btn">Explore Courses</a>
             </div>
           </div>
         </div>
@@ -114,7 +52,7 @@ class Membros extends React.Component {
 			      <div className="row justify-content-center">
 				      <div className="col-lg-8">
 					      <div className="section-title text-center">
-						      <h1>Membros</h1>
+						      <h1>Time de Desenvolvimento</h1>
 					      </div>
 				      </div>
 			      </div>
@@ -124,13 +62,223 @@ class Membros extends React.Component {
                   <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
                 </div>
                 <div className="meta-text text-center">
-                  <h4>Ethel Davis</h4>
-                  <p className="designation">Sr. Faculty Data Science</p>
+                  <h4>Caio Oliveira</h4>
+                  <p className="designation">Product Owner</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/caiooliv"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/p5.jpeg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Lucas Macêdo</h4>
+                  <p className="designation">Software Architect</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/lukassxp"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Lucas Siqueira</h4>
+                  <p className="designation">Scrum Master</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/lucassiqz"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Matheus Rodrigues</h4>
+                  <p className="designation">DevOps</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/matheus-rn"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row justify-content-center d-flex align-items-center">
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>João Pedro</h4>
+                  <p className="designation">Desenvolvedor</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/jpcirqueira"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Lucas Alexandre</h4>
+                  <p className="designation">Desenvolvedor</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/lucasA27"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Matheus de Cristo </h4>
+                  <p className="designation">Desenvolvedor</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/MatheusEstanislau"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                <div className="thumb d-flex justify-content-center">
+                  <img className="img-fluid" src="img/faculty/f1.jpg" alt=""/>
+                </div>
+                <div className="meta-text text-center">
+                  <h4>Moacir Júnior</h4>
+                  <p className="designation">Desenvolvedor</p>
+
+                  <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/MoacirMSJ"><i class="fa fa-github"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row justify-content-center d-flex align-items-center">
+                <div className="col-lg-3 col-md-6 col-sm-12 single-faculty">
+                  <div className="thumb d-flex justify-content-center">
+                    <img className="img-fluid" src="img/faculty/f2.jpg" alt=""/>
+                  </div>
+                  <div className="meta-text text-center">
+                    <h4>Renan Cristyan</h4>
+                    <p className="designation">Desenvolvedor</p>
+
+                    <div className="align-items-center justify-content-center d-flex">
+                      <a href="https://www.github.com/RCristyan"><i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>
+            </div>    
+          </div>
+	      </section>
+    );
+  }
+}
+
+class Features extends React.Component {
+  render(){
+    return(
+      <section className="feature-area">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8">
+              <div className="section-title text-center">
+                <h1>Features That Make Us Hero</h1>
+                <p>
+                  If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for
+                  as low as $.17 each.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="feature-inner row">
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-crown"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".1s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-briefcase"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".3s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-medall-alt"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".5s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-key"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".1s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-files"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".3s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div className="feature-item">
+                <i className="ti-headphone-alt"></i>
+                <h4>Lorem Ipsum</h4>
+                <div className="wow fadeIn" data-wow-duration="1s" data-wow-delay=".5s">
+                  <p>
+                    Lorem ipsum dolor sit amet consec tetur adipisicing elit, sed do eiusmod tempor incididunt labore.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-	      </section>
+        </div>
+      </section>
     );
   }
 }
@@ -140,11 +288,12 @@ class Index extends React.Component {
     return (
         <div>
           <div>
-          <section className="home-banner-area relative" style={{height: "37.2em"}}>
+          <section className="home-banner-area relative" style={{height: "38.7em"}}>
             <div className="container">
               <div className="row fullscreen d-flex align-items-center justify-content-center">
                 <div className="banner-content col-lg-8 col-md-12">
-                  <h1 className="wow fadeIn" data-wow-duration="4s">We Rank the Best Courses <br/> on the Web</h1>
+                  <div><br/><br/><br/><br/><br/><br/><br/></div>
+                  <h1 className="wow fadeIn" data-wow-duration="4s"> The standard Lorem Ipsum <br/> on the Web</h1>
                   <p className="text-white">
                     In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space
                     telescope.
@@ -158,6 +307,7 @@ class Index extends React.Component {
           </section>
           </div>
           <Meio />
+          <Features />
           <Membros />
           <div id="back-top">
 		        <a title="Go to Top" href="#"></a>
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,89 +9,7 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'Caio Oliveira',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars1.githubusercontent.com/u/23109243?s=400&v=4',
-    infoLink: 'https://www.github.com/caiooliv',
-    pinned: true,
-    role: 'Product Owner',
-  },
-  {
-    caption: 'Lucas Macêdo',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars2.githubusercontent.com/u/19879482?s=400&v=4',
-    infoLink: 'https://www.github.com/lukassxp',
-    pinned: true,
-    role: 'Arquiteto de Software',
-  },
-  {
-    caption: 'Lucas Siqueira',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars1.githubusercontent.com/u/23382026?s=400&v=4',
-    infoLink: 'https://www.github.com/lucassiqz',
-    pinned: true,
-    role: 'Scrum Master',
-  },
-  {
-    caption: 'Matheus Nascimento',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars1.githubusercontent.com/u/31085700?s=400&v=4',
-    infoLink: 'https://www.github.com/matheus-rn',
-    pinned: true,
-    role: 'DevOps',
-  },
-  {
-    caption: 'João Pedro',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars0.githubusercontent.com/u/31708472?s=400&v=4',
-    infoLink: 'https://www.github.com/jpcirqueira',
-    pinned: true,
-    role: 'Desenvolvedor',
-  },
-  {
-    caption: 'Lucas Alexandre',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars0.githubusercontent.com/u/44438409?s=400&v=4',
-    infoLink: 'https://www.github.com/lucasA27',
-    pinned: true,
-    role: 'Desenvolvedor',
-  },
-  {
-    caption: 'Matheus de Cristo',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars1.githubusercontent.com/u/44438591?s=400&v=4',
-    infoLink: 'https://www.github.com/MatheusEstanislau',
-    pinned: true,
-    role: 'Desenvolvedor',
-  },
-  {
-    caption: 'Moacir Júnior',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars3.githubusercontent.com/u/48539765?s=400&v=4',
-    infoLink: 'https://www.github.com/MoacirMSJ',
-    pinned: true,
-    role: 'Desenvolvedor',
-  },
-  {
-    caption: 'Renan Cristyan',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: 'https://avatars1.githubusercontent.com/u/39436190?s=400&v=4',
-    infoLink: 'https://www.github.com/RCristyan',
-    pinned: true,
-    role: 'Desenvolvedor',
-  },
-];
+const users = [];
 
 const siteConfig = {
   title: '+Monitoria', // Title for your website.
@@ -129,8 +47,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#1DA1F2',
-    secondaryColor: '#0E4C72',
+    primaryColor: '#3387BE',
+    secondaryColor: '#004F79',
   },
 
   /* Custom fonts for website */
