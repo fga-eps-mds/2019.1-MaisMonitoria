c233c066be8c7c9d0c6ece5347933ff385be9f37
Caio Oliveira de Moraes <caiooliv97@gmail.com>
2019-05-01 22:38:45 -0300

    [REFACTOR] Refatorado EAP e canvas.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,7 +37,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {search: true},
-    {doc: 'doc-richpicture', label: 'Requisitos'},
+    {doc: 'doc-backlog', label: 'Requisitos'},
     {doc: 'doc-tap', label: 'Docs'},
     {doc: 'sprint0-planning', label: 'Sprints'},
   //  {page: 'help', label: 'Help'},
